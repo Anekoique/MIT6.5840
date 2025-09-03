a2868bcb17e78d55528b2257fff94829a228c8f2
Anekoique <ctolu01@gmail.com>
2025-09-03 13:20:44 +0800

    feat(kvsrv): Impl Lock via KV Clerk

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,33 +1,63 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
+
+const UnLocked string = ""
 
 type Lock struct {
 	// IKVClerk is a go interface for k/v clerks: the interface hides
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
-	// You may add code here
+	ck       kvtest.IKVClerk
+	key      string
+	clientID string
 }
 
-// The tester calls MakeLock() and passes in a k/v clerk; your code can
+// MakeLock passes in a k/v clerk; your code can
 // perform a Put or Get by calling lk.ck.Put() or lk.ck.Get().
 //
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
-	// You may add code here
+	lk := &Lock{
+		ck:       ck,
+		key:      l,
+		clientID: kvtest.RandValue(8),
+	}
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Your code here
+	for {
+		value, version, ok := lk.ck.Get(lk.key)
+		if ok != rpc.OK && ok != rpc.ErrNoKey || value != UnLocked {
+			continue
+		}
+
+		if ok := lk.ck.Put(lk.key, lk.clientID, version); ok != rpc.OK {
+			continue
+		}
+
+		value, _, ok = lk.ck.Get(lk.key)
+		if ok == rpc.OK && value == lk.clientID {
+			break
+		}
+	}
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	for {
+		value, version, ok := lk.ck.Get(lk.key)
+		if ok != rpc.OK || value == UnLocked {
+			continue
+		}
+
+		if ok := lk.ck.Put(lk.key, UnLocked, version); ok == rpc.OK {
+			break
+		}
+	}
 }
